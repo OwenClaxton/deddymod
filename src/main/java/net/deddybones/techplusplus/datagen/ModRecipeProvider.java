2a82f9c5eda84aca1a4d47fafa3bd6743aaf0acc
Owen Claxton <owenclaxton@gmail.com>
2024-03-24 22:25:53 +1000

    first iteration of new recipes with billet system

--- src/main/java/net/deddybones/techplusplus/datagen/ModRecipeProvider.java
+++ src/main/java/net/deddybones/techplusplus/datagen/ModRecipeProvider.java
@@ -4,13 +4,16 @@ import com.google.common.collect.ImmutableList;
 import net.deddybones.techplusplus.TechPlusPlus;
 import net.deddybones.techplusplus.block.ModBlocks;
 import net.deddybones.techplusplus.item.ModItems;
+import net.deddybones.techplusplus.item.TweakedVanillaItems;
 import net.deddybones.techplusplus.util.crafting.EmptyRecipeBuilder;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.*;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 import org.jetbrains.annotations.NotNull;
 
@@ -18,8 +21,10 @@ import java.util.List;
 
 public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
     public static final ImmutableList<ItemLike> SAPPHIRE_SMELTABLES = ImmutableList.of(ModBlocks.SAPPHIRE_ORE.get(), ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
-    public static final ImmutableList<ItemLike> PLASTIMETAL_NUGGET_SMELTABLES = ImmutableList.of(ModItems.RAW_PLASTIMETAL.get(), ModBlocks.RUINED_PLASTIMETAL.get(), ModBlocks.DEEPSLATE_RUINED_PLASTIMETAL.get());
-    public static final ImmutableList<ItemLike> PLASTIMETAL_SMELTABLES = ImmutableList.of(ModBlocks.RAW_PLASTIMETAL_BLOCK.get());
+    public static final ImmutableList<ItemLike> PLASTIMETAL_BILLET_SMELTABLES = ImmutableList.of(ModItems.PLASTIMETAL_NUGGET.get(), ModBlocks.RUINED_PLASTIMETAL.get(), ModBlocks.DEEPSLATE_RUINED_PLASTIMETAL.get());
+    public static final ImmutableList<ItemLike> PLASTIMETAL_INGOT_SMELTABLES = ImmutableList.of(ModItems.RAW_PLASTIMETAL.get());
+    public static final Ingredient PLASTIMETAL_RECYCLABLES = Ingredient.of(ModItems.PLASTIMETAL_PICKAXE.get(), ModItems.PLASTIMETAL_SHOVEL.get(), ModItems.PLASTIMETAL_AXE.get(), ModItems.PLASTIMETAL_HOE.get(), ModItems.PLASTIMETAL_SWORD.get(), ModItems.PLASTIMETAL_HELMET.get(), ModItems.PLASTIMETAL_CHESTPLATE.get(), ModItems.PLASTIMETAL_LEGGINGS.get(), ModItems.PLASTIMETAL_BOOTS.get(), ModItems.PLASTIMETAL_HORSE_ARMOR.get());
+    public static final Ingredient IRON_RECYCLABLES = Ingredient.of(Items.IRON_PICKAXE, Items.IRON_SHOVEL, TweakedVanillaItems.IRON_AXE.get(), Items.IRON_HOE, Items.IRON_SWORD, Items.IRON_HELMET, Items.IRON_CHESTPLATE, Items.IRON_LEGGINGS, Items.IRON_BOOTS, Items.IRON_HORSE_ARMOR, Blocks.IRON_BARS, Blocks.IRON_DOOR, Blocks.LIGHT_WEIGHTED_PRESSURE_PLATE);
     public ModRecipeProvider(PackOutput pOutput) {
         super(pOutput);
     }
@@ -28,22 +33,28 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
     protected void buildRecipes(@NotNull RecipeOutput pOutput) {
         oreSmelting(pOutput, SAPPHIRE_SMELTABLES, RecipeCategory.MISC, ModItems.SAPPHIRE.get(), 1.0F, 200, "sapphire");
         oreBlasting(pOutput, SAPPHIRE_SMELTABLES, RecipeCategory.MISC, ModItems.SAPPHIRE.get(), 1.0F, 100, "sapphire");
-
-        oreSmelting(pOutput, PLASTIMETAL_NUGGET_SMELTABLES, RecipeCategory.MISC, ModItems.PLASTIMETAL_NUGGET.get(), 0.1F, 200, "plastimetal");
-        oreBlasting(pOutput, PLASTIMETAL_NUGGET_SMELTABLES, RecipeCategory.MISC, ModItems.PLASTIMETAL_NUGGET.get(), 0.1F, 100, "plastimetal");
-        oreSmelting(pOutput, PLASTIMETAL_SMELTABLES, RecipeCategory.MISC, ModItems.PLASTIMETAL_INGOT.get(), 1.0F, 200, "plastimetal");
-        oreBlasting(pOutput, PLASTIMETAL_SMELTABLES, RecipeCategory.MISC, ModItems.PLASTIMETAL_INGOT.get(), 1.0F, 100, "plastimetal");
-        nineThingsStorageRecipes(pOutput, RecipeCategory.MISC, ModItems.PLASTIMETAL_NUGGET.get(), RecipeCategory.MISC, ModItems.PLASTIMETAL_INGOT.get());
-
         nineThingsStorageRecipes(pOutput, RecipeCategory.MISC, ModItems.SAPPHIRE.get(), RecipeCategory.BUILDING_BLOCKS, ModBlocks.SAPPHIRE_BLOCK.get());
-        nineThingsStorageRecipes(pOutput, RecipeCategory.MISC, ModItems.PLASTIMETAL_INGOT.get(), RecipeCategory.BUILDING_BLOCKS, ModBlocks.PLASTIMETAL_BLOCK.get());
-        nineThingsStorageRecipes(pOutput, RecipeCategory.MISC, ModItems.RAW_PLASTIMETAL.get(), RecipeCategory.BUILDING_BLOCKS, ModBlocks.RAW_PLASTIMETAL_BLOCK.get());
 
+        oreSmelting(pOutput, PLASTIMETAL_BILLET_SMELTABLES, RecipeCategory.MISC, ModItems.PLASTIMETAL_BILLET.get(), 0.1F, 200, "plastimetal");
+        oreBlasting(pOutput, PLASTIMETAL_BILLET_SMELTABLES, RecipeCategory.MISC, ModItems.PLASTIMETAL_BILLET.get(), 0.1F, 100, "plastimetal");
+        oreSmelting(pOutput, PLASTIMETAL_INGOT_SMELTABLES, RecipeCategory.MISC, ModItems.PLASTIMETAL_INGOT.get(), 1.0F, 200, "plastimetal");
+        oreBlasting(pOutput, PLASTIMETAL_INGOT_SMELTABLES, RecipeCategory.MISC, ModItems.PLASTIMETAL_INGOT.get(), 1.0F, 100, "plastimetal");
+        nineThingsStorageRecipes(pOutput, RecipeCategory.MISC, ModItems.PLASTIMETAL_NUGGET.get(), RecipeCategory.MISC, ModItems.RAW_PLASTIMETAL.get());
+        nineThingsStorageRecipes(pOutput, RecipeCategory.MISC, ModItems.PLASTIMETAL_BILLET.get(), RecipeCategory.MISC, ModItems.PLASTIMETAL_INGOT.get());
+        nineThingsStorageRecipes(pOutput, RecipeCategory.MISC, ModItems.RAW_PLASTIMETAL.get(), RecipeCategory.BUILDING_BLOCKS, ModBlocks.RAW_PLASTIMETAL_BLOCK.get());
+        nineThingsStorageRecipes(pOutput, RecipeCategory.MISC, ModItems.PLASTIMETAL_INGOT.get(), RecipeCategory.BUILDING_BLOCKS, ModBlocks.PLASTIMETAL_BLOCK.get());
+
+        nineThingsStorageRecipes(pOutput, RecipeCategory.MISC, ModItems.TIN_NUGGET.get(), RecipeCategory.MISC, ModItems.RAW_TIN.get());
+        nineThingsStorageRecipes(pOutput, RecipeCategory.MISC, ModItems.TIN_BILLET.get(), RecipeCategory.MISC, ModItems.TIN_INGOT.get());
         nineThingsStorageRecipes(pOutput, RecipeCategory.MISC, ModItems.TIN_INGOT.get(), RecipeCategory.BUILDING_BLOCKS, ModBlocks.TIN_BLOCK.get());
         nineThingsStorageRecipes(pOutput, RecipeCategory.MISC, ModItems.RAW_TIN.get(), RecipeCategory.BUILDING_BLOCKS, ModBlocks.RAW_TIN_BLOCK.get());
 
+        nineThingsStorageRecipes(pOutput, RecipeCategory.MISC, ModItems.BRONZE_NUGGET.get(), RecipeCategory.MISC, ModItems.RAW_BRONZE.get());
+        nineThingsStorageRecipes(pOutput, RecipeCategory.MISC, ModItems.BRONZE_BILLET.get(), RecipeCategory.MISC, ModItems.BRONZE_INGOT.get());
         nineThingsStorageRecipes(pOutput, RecipeCategory.MISC, ModItems.BRONZE_INGOT.get(), RecipeCategory.BUILDING_BLOCKS, ModBlocks.BRONZE_BLOCK.get());
         nineThingsStorageRecipes(pOutput, RecipeCategory.MISC, ModItems.RAW_BRONZE.get(), RecipeCategory.BUILDING_BLOCKS, ModBlocks.RAW_BRONZE_BLOCK.get());
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.RAW_BRONZE.get(), 1).requires(ModItems.COPPER_BILLET.get(), 7).requires(ModItems.TIN_BILLET.get(), 2).unlockedBy("has_copper_billet", has(ModItems.COPPER_BILLET.get())).unlockedBy("has_tin_billet", has(ModItems.TIN_BILLET.get())).save(pOutput);
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModBlocks.RAW_BRONZE_BLOCK.get(), 1).requires(Items.COPPER_INGOT, 7).requires(ModItems.TIN_INGOT.get(), 2).unlockedBy("has_copper_ingot", has(Items.COPPER_INGOT)).unlockedBy("has_tin_ingot", has(ModItems.TIN_INGOT.get())).save(pOutput);
 
         nineThingsStorageRecipes(pOutput, RecipeCategory.MISC, Items.STICK, RecipeCategory.MISC, ModItems.STICK_BUNDLE.get());
 
@@ -51,8 +62,8 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
         doorBuilder(ModBlocks.PLASTIMETAL_DOOR.get(), Ingredient.of(ModItems.PLASTIMETAL_INGOT.get())).unlockedBy(getHasName(ModItems.PLASTIMETAL_INGOT.get()), has(Items.IRON_INGOT)).save(pOutput);
         twoByTwoPacker(pOutput, RecipeCategory.REDSTONE, ModBlocks.PLASTIMETAL_TRAPDOOR.get(), ModItems.PLASTIMETAL_INGOT.get());
 
-        SimpleCookingRecipeBuilder.smelting(Ingredient.of(ModItems.PLASTIMETAL_PICKAXE.get(), ModItems.PLASTIMETAL_SHOVEL.get(), ModItems.PLASTIMETAL_AXE.get(), ModItems.PLASTIMETAL_HOE.get(), ModItems.PLASTIMETAL_SWORD.get(), ModItems.PLASTIMETAL_HELMET.get(), ModItems.PLASTIMETAL_CHESTPLATE.get(), ModItems.PLASTIMETAL_LEGGINGS.get(), ModItems.PLASTIMETAL_BOOTS.get(), ModItems.PLASTIMETAL_HORSE_ARMOR.get()), RecipeCategory.MISC, ModItems.PLASTIMETAL_NUGGET.get(), 0.1F, 200).unlockedBy("has_plastimetal_pickaxe", has(ModItems.PLASTIMETAL_PICKAXE.get())).unlockedBy("has_plastimetal_shovel", has(ModItems.PLASTIMETAL_SHOVEL.get())).unlockedBy("has_plastimetal_axe", has(ModItems.PLASTIMETAL_AXE.get())).unlockedBy("has_plastimetal_hoe", has(ModItems.PLASTIMETAL_HOE.get())).unlockedBy("has_plastimetal_sword", has(ModItems.PLASTIMETAL_SWORD.get())).unlockedBy("has_plastimetal_helmet", has(ModItems.PLASTIMETAL_HELMET.get())).unlockedBy("has_plastimetal_chestplate", has(ModItems.PLASTIMETAL_CHESTPLATE.get())).unlockedBy("has_plastimetal_leggings", has(ModItems.PLASTIMETAL_LEGGINGS.get())).unlockedBy("has_plastimetal_boots", has(ModItems.PLASTIMETAL_BOOTS.get())).unlockedBy("has_plastimetal_horse_armor", has(ModItems.PLASTIMETAL_HORSE_ARMOR.get())).save(pOutput, TechPlusPlus.MOD_ID + ":" + getSmeltingRecipeName(ModItems.PLASTIMETAL_NUGGET.get()));
-        SimpleCookingRecipeBuilder.blasting(Ingredient.of(ModItems.PLASTIMETAL_PICKAXE.get(), ModItems.PLASTIMETAL_SHOVEL.get(), ModItems.PLASTIMETAL_AXE.get(), ModItems.PLASTIMETAL_HOE.get(), ModItems.PLASTIMETAL_SWORD.get(), ModItems.PLASTIMETAL_HELMET.get(), ModItems.PLASTIMETAL_CHESTPLATE.get(), ModItems.PLASTIMETAL_LEGGINGS.get(), ModItems.PLASTIMETAL_BOOTS.get(), ModItems.PLASTIMETAL_HORSE_ARMOR.get()), RecipeCategory.MISC, ModItems.PLASTIMETAL_NUGGET.get(), 0.1F, 100).unlockedBy("has_plastimetal_pickaxe", has(ModItems.PLASTIMETAL_PICKAXE.get())).unlockedBy("has_plastimetal_shovel", has(ModItems.PLASTIMETAL_SHOVEL.get())).unlockedBy("has_plastimetal_axe", has(ModItems.PLASTIMETAL_AXE.get())).unlockedBy("has_plastimetal_hoe", has(ModItems.PLASTIMETAL_HOE.get())).unlockedBy("has_plastimetal_sword", has(ModItems.PLASTIMETAL_SWORD.get())).unlockedBy("has_plastimetal_helmet", has(ModItems.PLASTIMETAL_HELMET.get())).unlockedBy("has_plastimetal_chestplate", has(ModItems.PLASTIMETAL_CHESTPLATE.get())).unlockedBy("has_plastimetal_leggings", has(ModItems.PLASTIMETAL_LEGGINGS.get())).unlockedBy("has_plastimetal_boots", has(ModItems.PLASTIMETAL_BOOTS.get())).unlockedBy("has_plastimetal_horse_armor", has(ModItems.PLASTIMETAL_HORSE_ARMOR.get())).save(pOutput, TechPlusPlus.MOD_ID + ":" + getBlastingRecipeName(ModItems.PLASTIMETAL_NUGGET.get()));
+        recycleIntoSomething(pOutput, PLASTIMETAL_RECYCLABLES, RecipeCategory.MISC, ModItems.PLASTIMETAL_BILLET.get(), 0.1F);
+        recycleIntoSomething(pOutput, IRON_RECYCLABLES, RecipeCategory.MISC, ModItems.IRON_BILLET.get(), 0.1F);
 
         ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ModItems.PLASTIMETAL_BOOTS.get()).define('X', ModItems.PLASTIMETAL_INGOT.get()).pattern("X X").pattern("X X").unlockedBy("has_plastimetal_ingot", has(ModItems.PLASTIMETAL_INGOT.get())).save(pOutput);
         ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ModItems.PLASTIMETAL_LEGGINGS.get()).define('X', ModItems.PLASTIMETAL_INGOT.get()).pattern("XXX").pattern("X X").pattern("X X").unlockedBy("has_plastimetal_ingot", has(ModItems.PLASTIMETAL_INGOT.get())).save(pOutput);
@@ -89,6 +100,26 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
 //        }
 //        removeRecipe(pOutput, "arrow");
 
+    }
+
+    protected static void recycleIntoSomething(@NotNull RecipeOutput pOutput, @NotNull Ingredient itemsForRecycling, @NotNull RecipeCategory recipeCategory,
+                                               @NotNull ItemLike recipeResult, float experience, String modid) {
+        SimpleCookingRecipeBuilder smeltingBuilder = SimpleCookingRecipeBuilder.smelting(itemsForRecycling, recipeCategory, recipeResult, experience, 200);
+        SimpleCookingRecipeBuilder blastingBuilder = SimpleCookingRecipeBuilder.blasting(itemsForRecycling, recipeCategory, recipeResult, experience, 100);
+
+        for (ItemStack itemStackToRecycle : itemsForRecycling.getItems()) {
+            Item itemToRecycle = itemStackToRecycle.getItem();
+            smeltingBuilder.unlockedBy("has_" + itemToRecycle.toString(), has(itemToRecycle));
+            blastingBuilder.unlockedBy("has_" + itemToRecycle.toString(), has(itemToRecycle));
+        }
+
+        smeltingBuilder.save(pOutput, modid + ":" + getSmeltingRecipeName(recipeResult));
+        blastingBuilder.save(pOutput, modid + ":" + getBlastingRecipeName(recipeResult));
+    }
+
+    protected static void recycleIntoSomething(@NotNull RecipeOutput pOutput, @NotNull Ingredient itemsForRecycling, @NotNull RecipeCategory recipeCategory,
+                                               @NotNull ItemLike recipeResult, float experience) {
+        recycleIntoSomething(pOutput, itemsForRecycling, recipeCategory, recipeResult, experience, TechPlusPlus.MOD_ID);
     }
 
     protected static void itemListAndItemLike(@NotNull RecipeOutput pOutput, @NotNull RecipeCategory recipeCategory,
